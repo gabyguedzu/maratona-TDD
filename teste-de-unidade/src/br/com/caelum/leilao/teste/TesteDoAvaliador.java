66b43e66759cbaff70b6967db87e2021d8ec31a5
Gabriela <zooiv3rde@gmail.com>
2019-12-16 21:12:38 -0300

    testes

--- teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -1,6 +1,9 @@
 package br.com.caelum.leilao.teste;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
@@ -25,11 +28,44 @@ public class TesteDoAvaliador {
 		Avaliador leiloeiro = new Avaliador();
 		leiloeiro.avalia(leilao);
 		
-		double maiorEsperado = 900;
-		double menorEsperado = 300;
+		assertEquals(900.00, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(300.00, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEntenderLancesComApenasUmLance() {
+		Usuario joao = new Usuario("João");
 		
-		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
-		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		Leilao leilao = new Leilao("HB20 Novo!!!!");
+		leilao.propoe(new Lance(joao, 1000.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.00001);
+		assertEquals(1000.0, leiloeiro.getMenorLance(), 0.00001);
+	}
+	
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		Usuario joao = new Usuario("João");
+		Usuario gabriela = new Usuario("Gabriela");
+		
+		Leilao leilao = new Leilao("HB20 Novo!!!!");
+		leilao.propoe(new Lance(joao, 1000.0));
+		leilao.propoe(new Lance(gabriela, 2000.0));
+		leilao.propoe(new Lance(joao, 3000.0));
+		leilao.propoe(new Lance(gabriela, 4000.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		
+		assertEquals(3, maiores.size());
+		assertEquals(4000.0, maiores.get(0).getValor(), 0.00001);
+		assertEquals(3000.0, maiores.get(1).getValor(), 0.00001);
+		assertEquals(2000.0, maiores.get(2).getValor(), 0.00001);
 	}
 }
 	

a64878fc8cf8502f536e45fe59a45722c79ae5c9
Gabriela <zooiv3rde@gmail.com>
2019-12-19 21:39:05 -0300

    iniciando mockito

--- teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
+++ teste-de-unidade/test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -1,8 +1,13 @@
 package br.com.caelum.leilao.servico;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -19,17 +24,16 @@ public class EncerradorDeLeilaoTest {
 		
 		Leilao leilao1 = new CriadorDeLeilao().para("TVzona").constroi();
 		Leilao leilao2 = new CriadorDeLeilao().para("Cama").constroi();
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao1, leilao2);
 		
-		//parei aqui
-		LeilaoDao dao = new LeilaoDao();
-		dao.salva(leilao1);
-		dao.salva(leilao2);
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+		when(daoFalso.correntes()).thenReturn(leiloesAntigos);
 		
-		EncerradorDeLeilao encerrador = new EncerradorDeLeilao();
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
 		encerrador.encerra();
 		
+		assertEquals(2, encerrador.getTotalEncerrados());
 		assertTrue(leilao1.isEncerrado());
 		assertTrue(leilao2.isEncerrado());
 	}
-
 }
